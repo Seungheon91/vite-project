7362ba31ef82b9625434dfb80452b130d13e1006
StarkLSH <lsh@zipdoc.kr>
2023-03-10 11:45:20 +0900

    composition api 활용 체크박스 전체 선택 및 해제 구현

--- src/components/composition/Checkbox.vue
+++ src/components/composition/Checkbox.vue
@@ -1,18 +1,51 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="checkbox"
+        v-model="allSelected"
+        id="selectAll"
+        @click="selectAll"
+      />
+      <label for="selectAll">전체 선택</label>
+    </div>
+    <div v-for="category in categories" :key="category.id">
+      <input
+        type="checkbox"
+        v-model="selectedCategories"
+        :value="category.id"
+        :id="category.id"
+      />
+      <label :for="category.id">{{ category.name }}</label>
+    </div>
+  </div>
+</template>
+
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, reactive, watch } from "vue";
 
-const count = ref(0);
+const categories = ref([
+  { id: 1, name: "사과" },
+  { id: 2, name: "배" },
+  { id: 3, name: "포도" },
+  { id: 4, name: "오렌지" },
+]);
+const selectedCategories = ref([]);
+const allSelected = ref(false);
 
-function increment() {
-  count.value++;
-}
-
-onMounted(() => {
-  console.log(`초기값${count}`);
+watch(selectedCategories, () => {
+  allSelected.value = selectedCategories.value.length === 4;
 });
+
+function selectAll() {
+  selectedCategories.value = [];
+
+  if (!allSelected.value) {
+    categories.value.forEach((category) => {
+      selectedCategories.value.push(category.id);
+    });
+  }
+}
 </script>
 
-<template>
-  <h1>하하하하</h1>
-  <button @click="increment">숫자 {{ count }}</button>
-</template>
+
